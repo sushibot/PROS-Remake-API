6df0b43e4cc0a2e66c481b2e6b6867614647bb0f
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2021-07-19 23:12:37 -1000

    feat: added new console log to display stuff

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 async function start_application() {
   const app = express();
   await run_application({ app });
-
+  console.log("This my new change");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
 }
 start_application();
